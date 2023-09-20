172e78daae6b1e21d36146e300575a361a051d10
dlp <elpibiomarkers@gmail.com>
2023-09-20 01:04:27 -0600

    Improve eeg plotting, console takes numerical keyboard as input to navigate

--- src/plot_eeg_marks.py
+++ src/plot_eeg_marks.py
@@ -2,42 +2,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from pynput import keyboard
-from pynput.keyboard import Key
-
-y_delta = 0
-
-
-def on_key_release(key):
-    if key == Key.right:
-        print("Right key clicked")
-    elif key == Key.left:
-        print("Left key clicked")
-    elif key == Key.up:
-        y_delta /= 2
-        print("Up key clicked")
-    elif key == Key.down:
-        y_delta *= 2
-        print("Down key clicked")
-    elif key == Key.esc:
-        exit()
-
-
-def plot_eeg_wdw(fn, time, data, mtg_labels, y_delta):
-    data = np.flipud(data)
-    mtg_labels = np.fliplr(mtg_labels)
-    y_ticks = []
-    nr_mtgs = len(mtg_labels)
-
-    for mi in range(0, nr_mtgs):
-        ch_offset = y_delta * mi * 2
-        y_ticks.append(ch_offset)
-        signal = ch_offset + data[mi]
-        plt.plot(time, signal, '-', color='black',  linewidth=0.5)
-
-    plt.xlim(min(time), max(time))
-    plt.yticks(y_ticks, labels=mtg_labels)
-    plt.title(fn)
-    plt.show(block=False)
+from pynput.keyboard import Key, Listener
 
 
 def plot_eeg_marks(eeg, marks):
@@ -50,9 +15,7 @@ def plot_eeg_marks(eeg, marks):
     units = eeg['units']
     time = eeg['time_s']
 
-    with keyboard.Listener(on_release=on_key_release) as listener:
-        listener.join()
-
+    wi = 0
     wdw_dur_s = 10
     wdw_nr_samples = int(math.floor(wdw_dur_s * fs))
 
@@ -60,9 +23,11 @@ def plot_eeg_marks(eeg, marks):
 
     y_delta = np.abs(np.max(data[:]))
 
+    fig, ax = plt.subplots()
+
     # for wi in range(0, len(wdw_start)):
-    wi = 0
     while 1:
+
         sample_start = wdw_start[wi]
         sample_end = sample_start + wdw_nr_samples
         wdw_data = data[:, sample_start:sample_end]
@@ -73,35 +38,43 @@ def plot_eeg_marks(eeg, marks):
         wdw_data = np.flipud(wdw_data)
         mtg_labels = np.fliplr(mtg_labels)
         y_ticks = []
+        ax.clear()
         for mi in range(0, nr_mtgs):
             ch_offset = y_delta * mi * 2
             y_ticks.append(ch_offset)
             signal = ch_offset + wdw_data[mi]
-            plt.plot(wdw_time, signal, '-', color='black',  linewidth=0.5)
+            ax.plot(wdw_time, signal, '-', color='black',  linewidth=0.5)
 
-        plt.xlim(min(wdw_time), max(wdw_time))
-        plt.yticks(y_ticks, labels=mtg_labels)
-        plt.title(fn)
+        textstr = str(round(y_delta*1000000)) + "uV"
+        props = dict(boxstyle='round', facecolor='white', alpha=0.5)
+        ax.text(0.95, 0.05, textstr, transform=ax.transAxes, fontsize=12,
+                verticalalignment='top', bbox=props)
+
+        ax.set_xlim(min(wdw_time), max(wdw_time))
+        ax.set_yticks(y_ticks, labels=mtg_labels)
+        ax.set_title(fn)
+        plt.draw()
         plt.show(block=False)
 
         action = input("Type command: ")
         if action == '4':
             wi -= 1
-            if wi < 0:
-                wi = 0
-            plt.clf()
+            ax.clear()
             print('You Pressed left!')
         if action == '6':
             wi += 1
-            if wi > len(wdw_start)-1:
-                wi = len(wdw_start)-1
-            plt.clf()
+            ax.clear()
             print('You Pressed right!')
         if action == '2':
-            y_delta *= 2
-            plt.clf()
+            y_delta += y_delta*0.1
+            ax.clear()
             print('You Pressed down!')
         if action == '8':
-            y_delta /= 2
-            plt.clf()
+            y_delta -= y_delta*0.1
+            ax.clear()
             print('You Pressed up!')
+
+        if wi < 0:
+            wi = 0
+        if wi > len(wdw_start)-1:
+            wi = len(wdw_start)-1
