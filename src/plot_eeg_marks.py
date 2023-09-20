d1a1fb7a11b458c5ba1ad15bbdf615b7c457dc02
dlp <elpibiomarkers@gmail.com>
2023-09-20 10:25:54 -0600

    - In EEG review window add two panels for the raw and filtered signals - Add sensitivity and scroll buttons

--- src/plot_eeg_marks.py
+++ src/plot_eeg_marks.py
@@ -1,11 +1,149 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from pynput import keyboard
-from pynput.keyboard import Key, Listener
+
+from matplotlib.widgets import Button
+from fir_bp_filter import filter_data
+
+filename = ""
+time = []
+data = []
+bp_data = []
+mtg_labels = []
+y_delta_big = []
+y_delta_small = []
+nr_mtgs = []
+
+wdw_start = []
+wdw_nr_samples = 0
+
+fig = []
+ax_big = []
+ax_small = []
+ax_spctrm = []
+
+
+def back_button(button):
+    global wi
+    wi -= 1
+    plot_big_wdw()
+    plot_small_wdw()
+
+
+def fwd_button(button):
+    global wi
+    wi += 1
+    plot_big_wdw()
+    plot_small_wdw()
+
+
+def dwn_button(button):
+    global y_delta_big
+    y_delta_big += y_delta_big*0.1
+    plot_big_wdw()
+
+
+def up_button(button):
+    global y_delta_big
+    y_delta_big -= y_delta_big*0.1
+    plot_big_wdw()
+
+
+def dwn_button_small(button):
+    global y_delta_small
+    y_delta_small += y_delta_small*0.1
+    plot_small_wdw()
+
+
+def up_button_small(button):
+    global y_delta_small
+    y_delta_small -= y_delta_small*0.1
+    plot_small_wdw()
+
+
+def plot_big_wdw():
+    global filename, mtg_labels
+    global time, data, y_delta_big, wdw_start, wdw_nr_samples, wi
+    global ax_big
+
+    nr_mtgs = len(mtg_labels)
+
+    if wi < 0:
+        wi = 0
+    if wi > len(wdw_start)-1:
+        wi = len(wdw_start)-1
+
+    sample_start = wdw_start[wi]
+    sample_end = sample_start + wdw_nr_samples
+    wdw_data = data[:, sample_start:sample_end]
+    wdw_time = time[sample_start:sample_end]
+
+    wdw_data = np.flipud(wdw_data)
+    mtg_labels = np.fliplr(mtg_labels)
+
+    y_ticks = []
+    ax_big.clear()
+    for mi in range(0, nr_mtgs):
+        ch_offset = y_delta_big * mi * 2
+        y_ticks.append(ch_offset)
+        signal = ch_offset + wdw_data[mi]
+        ax_big.plot(wdw_time, signal, '-', color='black',  linewidth=0.5)
+
+    textstr = str(round(y_delta_big*1000000)) + "uV"
+    props = dict(boxstyle='round', facecolor='white', alpha=0.5)
+    ax_big.text(0.9, 0.05, textstr, transform=ax_big.transAxes, fontsize=12,
+                verticalalignment='top', bbox=props)
+
+    ax_big.set_xlim(min(wdw_time), max(wdw_time))
+    ax_big.set_yticks(y_ticks, labels=mtg_labels)
+    ax_big.set_title(filename)
+    plt.draw()
+
+
+def plot_small_wdw():
+    global filename, mtg_labels
+    global time, bp_data, y_delta_small, wdw_start, wdw_nr_samples, wi
+    global ax_small
+
+    nr_mtgs = len(mtg_labels)
+
+    if wi < 0:
+        wi = 0
+    if wi > len(wdw_start)-1:
+        wi = len(wdw_start)-1
+
+    sample_start = wdw_start[wi]
+    sample_end = sample_start + wdw_nr_samples
+    wdw_data = bp_data[:, sample_start:sample_end]
+    wdw_time = time[sample_start:sample_end]
+
+    wdw_data = np.flipud(wdw_data)
+    mtg_labels = np.fliplr(mtg_labels)
+
+    y_ticks = []
+    ax_small.clear()
+    for mi in range(0, nr_mtgs):
+        ch_offset = y_delta_small * mi * 2
+        y_ticks.append(ch_offset)
+        signal = ch_offset + wdw_data[mi]
+        ax_small.plot(wdw_time, signal, '-', color='black',  linewidth=0.5)
+
+    textstr = str(round(y_delta_small*1000000)) + "uV"
+    props = dict(boxstyle='round', facecolor='white', alpha=0.5)
+    ax_small.text(0.9, 0.05, textstr, transform=ax_small.transAxes, fontsize=12,
+                  verticalalignment='top', bbox=props)
+
+    ax_small.set_xlim(min(wdw_time), max(wdw_time))
+    ax_small.set_yticks(y_ticks, labels=mtg_labels)
+    ax_small.set_title(filename)
+    plt.draw()
 
 
 def plot_eeg_marks(eeg, marks):
+    global filename, mtg_labels, time, data, bp_data
+    global y_delta_big, y_delta_small, wdw_start, wdw_nr_samples, wi
+    global fig, ax_big, ax_small, ax_spctrm
+
     filename = eeg['filename']
     fs = eeg['fs']
     mtg_labels = eeg['mtg_labels']
@@ -15,66 +153,52 @@ def plot_eeg_marks(eeg, marks):
     units = eeg['units']
     time = eeg['time_s']
 
+    bp_data = filter_data(fs, data, 80, 500)
+
     wi = 0
     wdw_dur_s = 10
     wdw_nr_samples = int(math.floor(wdw_dur_s * fs))
-
     wdw_start = np.arange(0, nr_samples, wdw_nr_samples)
+    y_delta_big = np.abs(np.max(data[:]))
+    y_delta_small = np.abs(np.max(bp_data[:]))
 
-    y_delta = np.abs(np.max(data[:]))
+    plt.close()
+    fig = plt.figure(figsize=(6, 6))
 
-    fig, ax = plt.subplots()
+    ax_big = fig.add_axes([0.05, 0.1, 0.45, 0.85])
+    ax_small = fig.add_axes([0.55, 0.1, 0.43, 0.85])
+    # ax_spctrm = fig.add_axes([0.02, 0.1, 0.5, 0.8])
 
-    # for wi in range(0, len(wdw_start)):
-    while 1:
+    axprev = fig.add_axes([0.45, 0.02, 0.05, 0.05])
+    axnext = fig.add_axes([0.5, 0.02, 0.05, 0.05])
+    axdown_big = fig.add_axes([0.2, 0.02, 0.05, 0.05])
+    axup_big = fig.add_axes([0.25, 0.02, 0.05, 0.05])
+    ax_down_small = fig.add_axes([0.7, 0.02, 0.05, 0.05])
+    ax_up_small = fig.add_axes([0.75, 0.02, 0.05, 0.05])
 
-        sample_start = wdw_start[wi]
-        sample_end = sample_start + wdw_nr_samples
-        wdw_data = data[:, sample_start:sample_end]
-        wdw_time = time[sample_start:sample_end]
+    bnext = Button(axnext, '>>')
+    bnext.on_clicked(fwd_button)
+    bprev = Button(axprev, '<<')
+    bprev.on_clicked(back_button)
 
-        # plot_eeg_wdw(filename, wdw_time, wdw_data, mtg_labels, y_delta)
-        fn = filename
-        wdw_data = np.flipud(wdw_data)
-        mtg_labels = np.fliplr(mtg_labels)
-        y_ticks = []
-        ax.clear()
-        for mi in range(0, nr_mtgs):
-            ch_offset = y_delta * mi * 2
-            y_ticks.append(ch_offset)
-            signal = ch_offset + wdw_data[mi]
-            ax.plot(wdw_time, signal, '-', color='black',  linewidth=0.5)
+    bdown = Button(axdown_big, '-')
+    bdown.on_clicked(dwn_button)
+    bup = Button(axup_big, '+')
+    bup.on_clicked(up_button)
 
-        textstr = str(round(y_delta*1000000)) + "uV"
-        props = dict(boxstyle='round', facecolor='white', alpha=0.5)
-        ax.text(0.95, 0.05, textstr, transform=ax.transAxes, fontsize=12,
-                verticalalignment='top', bbox=props)
+    bdown_small = Button(ax_down_small, '-')
+    bdown_small.on_clicked(dwn_button_small)
+    bup_small = Button(ax_up_small, '+')
+    bup_small.on_clicked(up_button_small)
 
-        ax.set_xlim(min(wdw_time), max(wdw_time))
-        ax.set_yticks(y_ticks, labels=mtg_labels)
-        ax.set_title(fn)
-        plt.draw()
-        plt.show(block=False)
+    plot_big_wdw()
+    plot_small_wdw()
 
-        action = input("Type command: ")
-        if action == '4':
-            wi -= 1
-            ax.clear()
-            print('You Pressed left!')
-        if action == '6':
-            wi += 1
-            ax.clear()
-            print('You Pressed right!')
-        if action == '2':
-            y_delta += y_delta*0.1
-            ax.clear()
-            print('You Pressed down!')
-        if action == '8':
-            y_delta -= y_delta*0.1
-            ax.clear()
-            print('You Pressed up!')
+    plt.show(block=True)
 
-        if wi < 0:
-            wi = 0
-        if wi > len(wdw_start)-1:
-            wi = len(wdw_start)-1
+
+"""     action = ""
+    while action != "exit":
+        stop = 1
+        # action = input("Exit(exit)? ")
+ """
